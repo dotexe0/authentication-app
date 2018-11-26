8b229a95a6f80186e358d076a4fdfe4467f123ea
Daniel Varela <dvarela@wiser.com>
2018-11-26 08:13:07 -0800

    add user signup route and contoller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,23 @@
-exports.signup = function(req, res, next) {
+const User = require('../models/user')
+
+exports.signup = async function(req, res, next) {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide an email and a password' })
+  }
   
+  const existingUser = await User.findOne({ email })
+    
+    if (existingUser) { 
+      return res.status(422).send({ error: 'Email is already in use' })
+    }
+    
+    let newUser = new User({ email, password })
+    
+    try {
+      await newUser.save()
+    } catch(err) {
+      return next(err)
+    }
+    res.json({ success: 'true'})
 }

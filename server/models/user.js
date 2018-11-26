9ccb631d3c6f672d1900d3f9f9cb7a1dddf3001c
Daniel Varela <dvarela@wiser.com>
2018-11-26 08:58:49 -0800

    salt passwords on pre-hook

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true, required: true },
   password: { type: String, required: true }
+})
+
+// on save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err)
+    }
+    
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err)
+      }
+      user.password = hash
+      next()
+    })
+  })
 })
 
 const ModelClass = mongoose.model('user', userSchema)

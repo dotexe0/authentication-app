ac0c39a15873c6239ef8f13d1e21bf9f9d251730
Daniel Varela <dvarela@wiser.com>
2018-11-27 10:57:43 -0800

    wire up redux store to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+import reducers from './reducers'
 import App from './components/App'
 import Welcome from './components/Welcome'
+import Signup from './components/auth/Signup'
 
 const app = (
+  <Provider store={createStore(reducers, {})}>
   <BrowserRouter>
     <App>
       <Route exact path="/" component={Welcome}/>
+      <Route exact path="/signup" component={Signup} />
     </App>
   </BrowserRouter>
+  </Provider>
 )
+
 ReactDOM.render(app, document.getElementById('root'))

c66bb229aa630fbb7fdeb9b2cb4e56b4dc245e7c
Daniel Varela <dvarela@wiser.com>
2018-11-27 15:31:01 -0800

    enhance provider with redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 import Welcome from './components/Welcome'
 import Signup from './components/auth/Signup'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(
+    applyMiddleware(reduxThunk))
+)
+
 const app = (
-  <Provider store={createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={store}>
   <BrowserRouter>
     <App>
       <Route exact path="/" component={Welcome}/>

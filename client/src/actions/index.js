02efc116d052a31303bf1b3c0020330de18550de
Daniel Varela <dvarela@wiser.com>
2018-11-28 09:13:21 -0800

    show error if email is in use

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import { AUTH_USER } from './types'
+import { AUTH_USER, AUTH_ERROR } from './types'
 
 // redux thunk allows us to return a function instead of an action object
 // like { type: AUTH_USER, payload: 'blahblah' }
-export const signup = (formProps) => async dispatch => {
-  const response = await axios.post('http://localhost:3090/signup', formProps)
-  dispatch({type: AUTH_USER, payload: response.data.token })
+export const signup = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signup', formProps)
+    dispatch({type: AUTH_USER, payload: response.data.token})
+    callback()
+  } catch(err) {
+    dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
+  }
 }

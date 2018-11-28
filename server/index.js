99e44a262ba12ad4734bd5bb35b8d0b6bd907de0
Daniel Varela <dvarela@wiser.com>
2018-11-28 08:01:34 -0800

    allow CORS

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true })
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json({type: '*/*'}))
+// npm module cors can simplify this code, but will leave here for deeper understanding of what
+// is really going on :)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 router(app)
 
 const port = process.env.PORT || 3090
